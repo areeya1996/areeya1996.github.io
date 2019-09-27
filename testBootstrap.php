9d5475c6f238d1c98ab2edf11bb1fef4ab3b8214
areeya1996 <39901747+areeya1996@users.noreply.github.com>
2019-09-27 15:21:52 +0700

    Update testBootstrap.php

--- testBootstrap.php
+++ testBootstrap.php
@@ -5,29 +5,22 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
 <div class="container-fluid">
-	<div class="row">
-		<div class="col">
-			<ul class="nav nav-pills nav-stacked">
-			  <li class="nav-item">
-			    <a class="nav-link active" href="#">CS Tool</a>
-			  </li>
-			  <li class="nav-item">
-			    <a class="nav-link" href="#">Admin Tool</a>
-			  </li>
-			  <li class="nav-item">
-			    <a class="nav-link" href="#">Report Tool</a>
-			  </li>
-			  <li class="nav-item">
-			    <a class="nav-link" href="#">Collection Tool</a>
-			  </li>
-			  <li class="nav-item">
-			    <a class="nav-link" href="#">User Management</a>
-			  </li>
-			</ul>
-		</div>
-		<div class="col-10">
-			
-		</div>
+	<div class="accordion" id="accordionExample">
+	  <div class="card">
+	    <div class="card-header" id="headingOne">
+	      <h2 class="mb-0">
+		<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+		  Collapsible Group Item #1
+		</button>
+	      </h2>
+	    </div>
+
+	    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+	      <div class="card-body">
+		Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+	      </div>
+	    </div>
+	  </div>
 	</div>
 </div>
 
